116679a1cc3b378e178ff69ef6ed6b2528ae3570
Danil Morozov <danilka0662@gmail.com>
2023-04-11 18:30:29 +0700

    fix header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
 import Cart from "./pages/Cart/Cart";
+import User from "./pages/User/User";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/common/Header/Header";
 import Footer from "./components/common/Footer/Footer";
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/user" element={<User />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
